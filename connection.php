bd75b3e826ad2693c80053c4d35a7cc4af94bc94
McAdam-Mathew <matthew.rufus29@gmail.com>
2020-07-14 08:13:48 +0530

    Today work

--- connection.php
+++ connection.php
@@ -1,20 +1,31 @@
 <?php
-$dbServername = 'localhost';
-$dbUsername = 'root';
-$dbPassword = '';
-$dbName = 'sample';
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "certificate";
 
-$name = $_POST['username'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+$name = $_POST['name'];
+$permit = $_POST['permit'];
+$company = $_POST['company'];
+$date = $_POST['date'];
 
-    $conn = new mysqli( 'localhost','root','',"sample") or die("Connection Error: ");
-	$sql = "INSERT into contact (name,email,subject,message) VALUES ('$name','$email','$subject','$message')";
+    
+echo "DB not connected";
+
+$conn = mysqli_connect('$dbServername','$dbUsername','$dbPassword','$dbName') or die("Connection Error: ");
+    if(!$conn)
+    {
+        echo "DB not connected";
+    }
+    else
+    {
+        echo "DB connected";
+    }
+    $sql = "INSERT into registration (name,date,permit,company) VALUES ('$name','$date','$permit','$company')";
     mysqli_query($conn, $sql);
 
 
-   header ("refresh:2; url=contact.html");
+   header ("refresh:2; url=questions.html");
 
-    require_once "contact.html";
+   
 ?>
